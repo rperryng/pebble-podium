96209f23202e16908fdf549dd29d84c0bae24302
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2015-10-04 12:32:01 -0400

    Add basic version of rising numbers

--- src/pebble-podium.c
+++ src/pebble-podium.c
@@ -10,26 +10,114 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   layer_mark_dirty(s_canvas_layer);
 }
 
-static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  
+static char* get_hours_text(struct tm *tick_time) {
   static char s_hours_buffer[] = "00";
   
   if (clock_is_24h_style()) {
     strftime(s_hours_buffer, sizeof("00"), "%H", tick_time);
   } else {
-    strftime(s_hours_buffer, sizeof("00"), "%M", tick_time);
+    strftime(s_hours_buffer, sizeof("00"), "%I", tick_time);
   }
   
+  return s_hours_buffer;
+}
+
+static char* get_minutes_text(struct tm *tick_time) {
+  static char s_minutes_buffer[] = "00"; 
+  strftime(s_minutes_buffer, sizeof("00"), "%M", tick_time);
+  return s_minutes_buffer;
+}
+
+static void draw_hours(GContext *ctx, struct tm *tick_time) {
+  char* hours_text = get_hours_text(tick_time);
+  GFont hours_font = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
+  graphics_context_set_text_color(ctx, GColorJazzberryJam);
+  
+  // Draw below canvas to bottom of canvas
+  for (int i = 0; i < 42; i++) {
+    graphics_draw_text(
+      ctx,
+      hours_text,
+      hours_font,
+      GRect(0, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 42),
+      GTextOverflowModeWordWrap,
+      GTextAlignmentCenter,
+      NULL);  
+  }
+  
+  // Draw from bottom of canvas to hour of day
+  uint8_t length_factor = ((tick_time->tm_hour * 144) / 24) + 42;
+  for (int i = 0; i < length_factor; i++) {
+    graphics_draw_text(
+      ctx,
+      hours_text,
+      hours_font,
+      GRect(0, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 34),
+      GTextOverflowModeWordWrap,
+      GTextAlignmentCenter,
+      NULL);
+  }
+  
+  // Draw time in white.
+  graphics_context_set_text_color(ctx, GColorWhite);
   graphics_draw_text(
-    ctx, 
-    s_hours_buffer, 
-    fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD), 
-    GRect(0, 48, s_window_bounds.size.w, 48), 
-    GTextOverflowModeWordWrap, 
-    GTextAlignmentCenter, 
+    ctx,
+    hours_text,
+    hours_font,
+    GRect(0, s_window_bounds.size.h - length_factor, s_window_bounds.size.w / 2, 34),
+    GTextOverflowModeWordWrap,
+    GTextAlignmentCenter,
     NULL);
+}
+
+static void draw_minutes(GContext *ctx, struct tm *tick_time) {
+  char* minutes_text = get_minutes_text(tick_time);
+  GFont minutes_font = fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS);
+  graphics_context_set_text_color(ctx, GColorTiffanyBlue); 
+  
+  // Draw below canvas to bottom of canvas
+  for (int i = 0; i < 34; i++) {
+    graphics_draw_text(
+      ctx,
+      minutes_text,
+      minutes_font,
+      GRect(s_window_bounds.size.w / 2, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 34),
+      GTextOverflowModeWordWrap,
+      GTextAlignmentCenter,
+      NULL);  
+  }
+  
+  // Draw from bottom of canvas to hour of day
+  uint8_t length_factor = ((tick_time->tm_min * 144) / 60) + 42;
+  for (int i = 0; i < length_factor; i++) {
+    graphics_draw_text(
+      ctx,
+      minutes_text,
+      minutes_font,
+      GRect(s_window_bounds.size.w / 2, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 34),
+      GTextOverflowModeWordWrap,
+      GTextAlignmentCenter,
+      NULL);
+  }
+  
+  // Draw time in white.
+  graphics_context_set_text_color(ctx, GColorWhite);
+  graphics_draw_text(
+    ctx,
+    minutes_text,
+    minutes_font,
+    GRect(s_window_bounds.size.w / 2, s_window_bounds.size.h - length_factor, s_window_bounds.size.w / 2, 34),
+    GTextOverflowModeWordWrap,
+    GTextAlignmentCenter,
+    NULL);
+}
+
+static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  draw_hours(ctx, tick_time);
+  draw_minutes(ctx, tick_time);
 }
 
 static void main_window_load(Window *window) {
