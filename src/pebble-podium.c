88c7e66121c03aef57994a7b129924d348876479
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2015-10-04 13:14:05 -0400

    Add minutes, replace some magic numbers with variables

--- src/pebble-podium.c
+++ src/pebble-podium.c
@@ -33,26 +33,28 @@ static void draw_hours(GContext *ctx, struct tm *tick_time) {
   GFont hours_font = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
   graphics_context_set_text_color(ctx, GColorJazzberryJam);
   
+  uint8_t text_height = 42;
+  
   // Draw below canvas to bottom of canvas
   for (int i = 0; i < 42; i++) {
     graphics_draw_text(
       ctx,
       hours_text,
       hours_font,
-      GRect(0, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 42),
+      GRect(0, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, text_height),
       GTextOverflowModeWordWrap,
       GTextAlignmentCenter,
-      NULL);  
+      NULL); 
   }
   
   // Draw from bottom of canvas to hour of day
-  uint8_t length_factor = ((tick_time->tm_hour * 144) / 24) + 42;
+  uint8_t length_factor = ((tick_time->tm_hour * 144) / 24) + text_height;
   for (int i = 0; i < length_factor; i++) {
     graphics_draw_text(
       ctx,
       hours_text,
       hours_font,
-      GRect(0, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 34),
+      GRect(0, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, text_height),
       GTextOverflowModeWordWrap,
       GTextAlignmentCenter,
       NULL);
@@ -64,7 +66,7 @@ static void draw_hours(GContext *ctx, struct tm *tick_time) {
     ctx,
     hours_text,
     hours_font,
-    GRect(0, s_window_bounds.size.h - length_factor, s_window_bounds.size.w / 2, 34),
+    GRect(0, s_window_bounds.size.h - length_factor, s_window_bounds.size.w / 2, text_height),
     GTextOverflowModeWordWrap,
     GTextAlignmentCenter,
     NULL);
@@ -73,28 +75,30 @@ static void draw_hours(GContext *ctx, struct tm *tick_time) {
 static void draw_minutes(GContext *ctx, struct tm *tick_time) {
   char* minutes_text = get_minutes_text(tick_time);
   GFont minutes_font = fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS);
-  graphics_context_set_text_color(ctx, GColorTiffanyBlue); 
+  graphics_context_set_text_color(ctx, GColorTiffanyBlue);
+  
+  uint8_t text_height = 34;
   
   // Draw below canvas to bottom of canvas
-  for (int i = 0; i < 34; i++) {
+  for (int i = 0; i < text_height; i++) {
     graphics_draw_text(
       ctx,
       minutes_text,
       minutes_font,
-      GRect(s_window_bounds.size.w / 2, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 34),
+      GRect(s_window_bounds.size.w / 2, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, text_height),
       GTextOverflowModeWordWrap,
       GTextAlignmentCenter,
-      NULL);  
+      NULL);
   }
   
   // Draw from bottom of canvas to hour of day
-  uint8_t length_factor = ((tick_time->tm_min * 144) / 60) + 42;
+  uint8_t length_factor = ((tick_time->tm_min * 144) / 60) + text_height;
   for (int i = 0; i < length_factor; i++) {
     graphics_draw_text(
       ctx,
       minutes_text,
       minutes_font,
-      GRect(s_window_bounds.size.w / 2, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, 34),
+      GRect(s_window_bounds.size.w / 2, s_window_bounds.size.h - i, s_window_bounds.size.w / 2, text_height),
       GTextOverflowModeWordWrap,
       GTextAlignmentCenter,
       NULL);
@@ -113,6 +117,8 @@ static void draw_minutes(GContext *ctx, struct tm *tick_time) {
 }
 
 static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+  graphics_context_set_antialiased(ctx, true);
+  
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
   
